f94e50769e2739fbd5ed33750d825a4e4b287526
tj-bu <tj-bu@DESKTOP-3NHNTGE>
2022-07-28 16:59:37 +0900

    123123123

--- gitEx/src/gitEx/gitEx.java
+++ gitEx/src/gitEx/gitEx.java
@@ -4,7 +4,7 @@ public class gitEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello JAVA 12312311");
+		System.out.println("Hello JAVA 12312311123123123123");
 	}
 
 }
